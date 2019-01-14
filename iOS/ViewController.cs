e4c366e94100cba96c7c9e09d661f87918b1926c
monojit18 <datta.monojit@gmail.com>
2019-01-14 10:53:23 +0530

    Modified to 85

--- iOS/ViewController.cs
+++ iOS/ViewController.cs
@@ -18,7 +18,7 @@ namespace TestAppCenter.iOS
 
             try
             {
-                int divByZero = 75 / int.Parse("0");
+                int divByZero = 85 / int.Parse("0");
             }
             catch (DivideByZeroException ex)
             {
