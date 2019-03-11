d5435735db761c0a27ad5f433fd5277c945b19bc
monojit18 <datta.monojit@gmail.com>
2019-03-11 13:18:20 +0530

    changed to 102

--- iOS/ViewController.cs
+++ iOS/ViewController.cs
@@ -18,7 +18,7 @@ namespace TestAppCenter.iOS
 
             try
             {
-                int divByZero = 101 / int.Parse("0");
+                int divByZero = 102 / int.Parse("0");
             }
             catch (DivideByZeroException ex)
             {
